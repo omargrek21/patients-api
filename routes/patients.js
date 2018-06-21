7334356df87b76ab2dd966b1b26300bd38ff4b1e
Omar Perez <omarmk6@gmail.com>
2018-06-21 00:43:19 -0400

    connection to db on get route

--- routes/patients.js
+++ routes/patients.js
@@ -1,8 +1,16 @@
 var express = require('express'),
-    router = express.Router();
+    router = express.Router(),
+    db = require('../models');
 
 router.get('/', function(req,res) {
-   res.send("hello from patients routes"); 
+   //res.send("hello from patients routes"); 
+   db.Patient.find()
+   .then(function(patients){
+       res.json(patients);
+   })
+   .catch(function(err){
+        res.send(err);
+   });
 });
 
 module.exports = router;
