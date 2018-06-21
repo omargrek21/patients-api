4b099946a81dec9c703c102cbe196cba28b6ab6a
Omar Perez <omarmk6@gmail.com>
2018-06-20 23:06:15 -0400

    server running

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 80;
+    port = process.env.PORT || 3000;
 
-    app.get('/', function(req, res){
-        res.send("hello from aws");
-    });
+app.get('/', function(req, res){
+    res.send("hello from aws");
+});
 
-    app.listen(port, function(){
-        console.log("APP IS RUNNING ON PORT " + port);
-    })
+app.listen(port, function(){
+    console.log("APP IS RUNNING ON PORT " + port);
+})
